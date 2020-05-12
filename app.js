d5a3bb28b1b7be74a7021446d82b32f739adeb78
eliasihai <eliasihai@hotmail.com>
2020-05-13 00:06:45 +0300

    another page starting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
     //Init Controller
@@ -8,7 +9,7 @@ function animateSlides() {
     const sliders = document.querySelectorAll('.slide');
     const nav = document.querySelectorAll('.nav-header');
     //Loop over each slide
-    sliders.forEach(slide => {
+    sliders.forEach((slide, index, slides) => {
         const revealImg = slide.querySelector('.reveal-img');
         const img = slide.querySelector('img');
         const revealText = slide.querySelector('.reveal-text');
@@ -19,11 +20,97 @@ function animateSlides() {
                 ease: 'power2.inOut'
             }
         })
-        slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '+=0.3')
-        slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
-        slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '+=0.5');
-        slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" } , '-=0.5');
-        
+        slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" }, '-=0.5');
+        slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '-=0.5');
+        slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, '-=0.5');
+        slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.5')
+        //Create Scene
+        slideScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            triggerHook: 0.25,
+            reverse: false
+        })
+            .setTween(slideTl)
+            .addIndicators({
+                colorStart: 'white',
+                colorTrigger: 'white',
+                name: 'slide'
+            })
+            .addTo(controller)
+
+        //New Animation
+        const pageTl = gsap.timeline();
+        let nextSlide = slides.length - 1 === index ? 'end' : sliders[index + 1];
+        pageTl.fromTo(nextSlide, { y: '0%' }, { y: '50%' })
+        pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 })
+        pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' }, '-=0.5')
+        //Create page scene
+        pageScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            duration: '100%',
+            triggerHook: 0,
+        }).addIndicators({
+            colorStart: 'white',
+            colorTrigger: 'white',
+            name: 'page',
+            indent: 200 //Moving 200px from the right side
+        })
+            .setPin(slide, { pushFollowers: false })
+            .setTween(pageTl)
+            .addTo(controller)
     });
 }
+
+const mouse = document.querySelector('.cursor');
+const mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector('.burger')
+
+function cursor(event) {
+    mouse.style.top = event.pageY + 'px';
+    mouse.style.left = event.pageX + 'px'
+}
+
+function activeCursor(event) {
+    const item = event.target;
+    if (item.id === 'logo' || item.classList.contains('burger')) {
+        mouse.classList.add('nav-active');
+    } else {
+        mouse.classList.remove('nav-active');
+    }
+    if (item.classList.contains('explore')) {
+        mouse.classList.add('explore-active');
+        gsap.to('.title-swipe', 1, { y: '0%' });
+        mouseTxt.innerText = 'Tap';
+    } else {
+        mouse.classList.remove('explore-active');
+        mouseTxt.innerText = '';
+        gsap.to('.title-swipe', 1, { y: '100%' });
+    }
+}
+
+function navToggle(event) {
+    if (!event.target.classList.contains('active')) {
+        event.target.classList.add('active')
+        gsap.to('.line1', 0.6, { rotate: '45deg', y: 5, background: 'black' })
+        gsap.to('.line2', 0.6, { rotate: '-45deg', y: -5, background: 'black' })
+        gsap.to('#logo', 1, { color: '#000' })
+        gsap.to('.nav-bar', 1, { clipPath: 'circle(2500px at 100% -10%)' })
+        document.body.classList.add('hide')
+    }
+    else {
+        event.target.classList.remove('active')
+        gsap.to('.line1', 0.6, { rotate: '0deg', y: 0, background: '#fff' })
+        gsap.to('.line2', 0.6, { rotate: '0deg', y: 0, background: '#fff' })
+        gsap.to('#logo', 1, { color: '#fff' })
+        gsap.to('.nav-bar', 1, { clipPath: 'circle(50px at 100% -10%)' })
+        document.body.classList.remove('hide')
+    }
+}
+
+
+//EventListeners
+burger.addEventListener('click', navToggle)
+window.addEventListener('mousemove', cursor);
+window.addEventListener('mouseover', activeCursor)
+
 animateSlides();
