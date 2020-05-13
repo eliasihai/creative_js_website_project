262f590fe6d8eb3765165a545df9bc7e54c4a6ac
eliasihai <eliasihai@hotmail.com>
2020-05-13 20:01:10 +0300

    scroll effects fashion page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let controller;
 let slideScene;
 let pageScene;
+let detailScene;
 
 function animateSlides() {
     //Init Controller
@@ -40,7 +41,7 @@ function animateSlides() {
 
         //New Animation
         const pageTl = gsap.timeline();
-        let nextSlide = slides.length - 1 === index ? 'end' : sliders[index + 1];
+        let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
         pageTl.fromTo(nextSlide, { y: '0%' }, { y: '50%' })
         pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 })
         pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' }, '-=0.5')
@@ -107,10 +108,112 @@ function navToggle(event) {
     }
 }
 
+//Barba Page Transitions
+const logo = document.querySelector('#logo');
+barba.init({
+    views: [
+        {
+            namespace: 'home',
+            beforeEnter() {
+                animateSlides();
+                logo.href = './index.html'
+            },
+            beforeLeave() {
+                controller.destroy();
+                slideScene.destroy();
+                pageScene.destroy();
+            }
+        },
+        {
+            namespace: 'fashion',
+            beforeEnter() {
+                logo.href = '../index.html';
+                detailAnimation();
+                gsap.fromTo('.nav-header',
+                    1,
+                    { y: '100%' },
+                    { y: '0%', ease: 'power2.inOut' }
+                );
+            },
+            beforeLeave(){
+                controller.destroy();
+                detailScene.destroy();
+            }
+        }
+    ],
+    transitions: [
+        {
+            leave({ current, next }) {
+                let done = this.async();
+                //Scroll to the top
+                window.scrollTo(0, 0)
+                //An Animation 
+                const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+                tl.fromTo(current.container,
+                    1,
+                    { opacity: 1 },
+                    { opacity: 0 }
+                );
+                tl.fromTo('.swipe',
+                    0.75,
+                    { x: '-100%' },
+                    { x: '0%', onComplete: done },
+                    "-=0.5"
+                );
+
+            },
+            enter({ current, next }) {
+                let done = this.async();
+                //An Animation 
+                const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+                tl.fromTo('.swipe',
+                    0.75,
+                    { x: '0%' },
+                    {
+                        x: '100%',
+                        stagger: 0.25,
+                        onComplete: done
+                    }
+                );
+                tl.fromTo(next.container,
+                    1,
+                    { opacity: 0 },
+                    { opacity: 1 }
+                );
+            }
+        }
+    ]
+})
+
+function detailAnimation() {
+    controller = new ScrollMagic.Controller();
+    const slides = document.querySelectorAll('.detail-slide');
+
+    slides.forEach((slide, index, slides) => {
+        const slideTl = gsap.timeline({ defaults: { duration: 1 } });
+        let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
+        const nextImg = nextSlide.querySelector('img');
+        slideTl.fromTo(slide, { opacity: 1 }, { opacity: 0 });
+        slideTl.fromTo(nextSlide, { opacity: 0 }, { opacity: 1 }, '-=1');
+        slideTl.fromTo(nextImg, { x: '50%' }, { x: '0%' },)
+        //Scene
+        detailScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            duration: '100%',
+            triggerHook: 0
+        })
+            .setPin(slide, { pushFollowers: false })
+            .setTween(slideTl)
+            .addIndicators({
+                colorStart: 'white',
+                colorTrigger: 'white',
+                name: 'detailScene'
+            })
+            .addTo(controller)
+    });
+}
 
 //EventListeners
 burger.addEventListener('click', navToggle)
 window.addEventListener('mousemove', cursor);
-window.addEventListener('mouseover', activeCursor)
-
-animateSlides();
+window.addEventListener('mouseover', activeCursor);
